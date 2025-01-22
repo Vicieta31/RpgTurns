e8cbf897a1393e7d954b93518b95a9d2ebc25127
Vicieta31 <danicastillor04@gmail.com>
2025-01-22 16:47:07 +0100

    Creando logica de turnos
    Ahora mismo estoy pillado en que pasa cuando 2 jugadores tienen ambos la misma velocidad, como hacer que primero vaya uno y luego otro

--- PruebaTurnosRpg/Assets/CalcularSpeedTurnos.cs
+++ PruebaTurnosRpg/Assets/CalcularSpeedTurnos.cs
@@ -5,31 +5,83 @@ using System.Numerics;
 using UnityEngine;
 using TMPro;
 using System;
+using System.Buffers.Text;
 
 public class CalcularSpeedTurnos : MonoBehaviour
 {
     public TextMeshProUGUI pepe;
 
     public List<int> Speeds;
-    List<float> AVInitial = new List<float>();
+    private List<KeyValuePair<int, float>> AvCharacter = new List<KeyValuePair<int, float>>();
+    private Dictionary<int, float> BaseAVs = new Dictionary<int, float>(); // Guardar valores base de acción
 
     int AV_Div = 10000;
+    private bool various0Av = false;
+    private int CurrentTurnPlayer = -1;
 
     void Start()
     {
-        for (int i = 0; i < Speeds.Last(); i++)
+        for (int i = 0; i < Speeds.Count(); i++)
         {
             int temp = AV_Div / Speeds.ElementAt(i);
-            AVInitial.Add(temp);
+            AvCharacter.Add(new KeyValuePair<int, float>(i, temp));
+            BaseAVs[i] = temp;
             Debug.Log(temp);
-            pepe.text += "\nPlayer " + i + ": " + temp;
         }
+        AvCharacter = AvCharacter.OrderBy(pair => pair.Value).ToList();
+
+        ShowAV();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (various0Av == false)
+            {
+                NextTurn();
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+    void NextTurn()
+    {
+        pepe.text = "";
+        float tempAV = AvCharacter[0].Value;
+        CurrentTurnPlayer = AvCharacter[0].Key;
+
+        for (int i = 0; i < AvCharacter.Count; i++)
+        {
+            var pair = AvCharacter[i];
+            float newValue = pair.Value - tempAV;
+
+            if (newValue <= 0)
+            {
+                newValue = BaseAVs[pair.Key]; // Reiniciar al valor base
+            }
+            AvCharacter[i] = new KeyValuePair<int, float>(pair.Key, newValue);
+        }
+
+        AvCharacter = AvCharacter.OrderBy(pair => pair.Value).ToList();
+
+        // Mostrar quién tiene el turno actual
+        ShowAV();
+    }
+
+
+    void ShowAV()
+    {
+        pepe.text = "";
+        pepe.text = $"<b>Turno del jugador: {CurrentTurnPlayer}</b>\n" + pepe.text;
+        foreach (var pair in AvCharacter)
+        {
+            pepe.text += $"Player {pair.Key}: {pair.Value}\n";
+        }
     }
 }
